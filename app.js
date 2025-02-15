efa69edf84e4ec1eecb6d62333302b25375bebbb
oste3224 <oste3224@.uni.sydney.edu.au>
2025-02-15 14:44:32 +1100

    stuff

---
+++ app.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+    if (window.location.pathname.includes('quote.html')) {
+        document.getElementById('quote-button').style.display = 'none';
+    }
+});
